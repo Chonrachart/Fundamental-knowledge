db3099105f7643caea4696c408532640a03b3d35
Chonrachart <chonrachart.sieng@gmail.com>
2026-02-18 10:55:05 +0700

    add script to install zabbix-agent2

---
+++ 00_Foundations/Bash-script/zabbix-agent2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+    echo "No root privilege"
+    exit 1
+fi
+
+if ! dpkg -s zabbix-release >/dev/null 2>&1; then
+    wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.0+ubuntu24.04_all.deb
+    dpkg -i zabbix-release_latest_7.0+ubuntu24.04_all.deb
+fi
+
+apt update
+apt install -y zabbix-agent2 \
+               zabbix-agent2-plugin-mongodb \
+               zabbix-agent2-plugin-mssql \
+               zabbix-agent2-plugin-postgresql
+
+systemctl enable zabbix-agent2
+systemctl restart zabbix-agent2
+
+
+conf=/etc/zabbix/zabbix_agent2.conf
+
+[ -f "$conf.bkp" ] || cp "$conf" "$conf.bkp" 
+
+sed -i 's/^[[:space:]]*Server=.*/Server=10.100.75.45/' "$conf"
+sed -i 's/^[[:space:]]*ServerActive=.*/ServerActive=10.100.75.45:10051/' "$conf"
+sed -i 's/^[[:space:]]*Hostname=.*/Hostname=mostestmonitor/' "$conf"
