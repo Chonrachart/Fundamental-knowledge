0aba04fe76eed5ae4e196da94dc9b1779a270d3b
Chonrachart <chonrachart.sieng@gmail.com>
2026-02-18 10:15:55 +0700

    add chrony synchronized

--- 00_Foundations/Bash-script/install-ntp.sh
+++ 00_Foundations/Bash-script/install-ntp.sh
@@ -11,12 +11,20 @@ conf=/etc/chrony/chrony.conf
 
 apt update
 apt install -y chrony 
-sed -i.bkp '/^[[:space:]]*pool/ s/^/#' "$conf"
+sed -i.bkp '/^[[:space:]]*pool/ s/^/#/' "$conf"
 grep -q "^[[:space:]]*server 192.168.10.254 iburst" "$conf" || echo "server 192.168.10.254 iburst" >> "$conf"
 
 systemctl enable chrony
 systemctl restart chrony
+chronyc makestep
 
+for i in {1..10}; do
+    if chronyc sources | grep -q "^\^\*"; then 
+        echo "Chrony synchronized"
+        break
+    fi
+    sleep 1
+done
 
 #verify 
 chronyc tracking
