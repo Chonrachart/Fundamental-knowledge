c1e578fa0c21c5e131422ffb3c9d8b5b215cf074
Chonrachart <chonrachart.sieng@gmail.com>
2026-02-18 11:55:20 +0700

    add install-kvm.sh file

---
+++ 00_Foundations/Bash-script/install-kvm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+    echo "No root privilege"
+    exit 1
+fi
+
+if [ egrep -c '(vmx|svm)' /proc/cpuinfo -gt 0 ]; then
+    echo "Virtualization not supported(or disabled in BIOS)"
+    exit 1 
+fi
+
+apt update
+apt install -y qemu-kvm \
+                libvirt-daemon-system \
+                libvirt-clients \
+                bridge-utils \
+                virtinst \
+                virt-manager
+
+usermod -aG libvirt $SUDO_USER
+usermod -aG kvm $SUDO_USER
+
+systemctl enable libvirtd
+systemctl restart libvirtd
+
+
+
