9c2015497d0a675c624de519466b00bfeb25a627
Chonrachart <chonrachart.sieng@gmail.com>
2026-02-23 14:55:47 +0700

    add install-java

---
+++ 00_Foundations/Shell-script/Install/install-java.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+INSTALL_DIR="/opt/java"
+INSTALL_TARGET="$INSTALL_DIR/jdk-"
+TAR_FILE="$INSTALL_DIR/jdk-21_linux-x64_bin.tar.gz"
+URL="https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz"
+
+check_root() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "No root privilege"
+        exit 1
+    fi
+}
+
+download_gz() {
+    if [ ! -f "$TAR_FILE" ]; then
+        wget -P "$INSTALL_DIR" "$URL"
+    else
+        echo "$TAR_FILE already exists"
+    fi
+}
+
+make_dir() {
+    mkdir -p "$INSTALL_DIR"
+}
+
+extract_java() {
+    if [ ! -d "$INSTALL_TARGET" ]; then
+        echo "Extracting Java..."
+        tar -xzf "$TAR_FILE" -C "$INSTALL_DIR"
+        mv "$INSTALL_DIR"/jdk-21.* "$INSTALL_TARGET"
+    else
+        echo "Java already installed."
+    fi
+}
+
+verify_java() {
+    "$INSTALL_TARGET/bin/java" -version
+}
+
+main() {
+    check_root
+    download_gz
+    make_dir
+    extract_java
+    verify_java
+    echo "Please see the exactly java version manualy"
+}
+
+main "$@"
