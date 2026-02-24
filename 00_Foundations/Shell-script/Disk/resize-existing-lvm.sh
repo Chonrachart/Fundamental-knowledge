7448859b635e0b1765de55699b4c43d084930167
Chonrachart <chonrachart.sieng@gmail.com>
2026-02-24 09:56:06 +0700

    Refactored resize-existing-lvm.sh

--- 00_Foundations/Shell-script/Disk/resize-existing-lvm.sh
+++ 00_Foundations/Shell-script/Disk/resize-existing-lvm.sh
@@ -1,17 +1,49 @@
-# this should see new add disk space
-lsblk
+#!/bin/bash
 
-# this update partition that want to resize  
-growpart /dev/sdx <partition> 
+set -e
 
-# this tell pv that partition was resize
-pvresize /dev/sdxx
+check_root() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "No root privilege"
+        exit 1
+    fi
+}
 
-# verify
-pvs
+check_new_disk() {
+    # this should see new add disk space
+    lsblk
+}
 
-# this tell lv to use free pv
-lvextend -L +10G /dev/[vg]/[lv]
+resize_partition() {
+    # this update partition that want to resize  
+    growpart /dev/sdx <partition>
+}
 
-# df -h stil see old disk space we need to tell file system
-resize2fs /dev/[vg]/[lv]
+resize_pv() {
+    # this tell pv that partition was resize
+    pvresize /dev/sdxx
+
+    # verify
+    pvs
+}
+
+extend_lv() {
+    # this tell lv to use free pv
+    lvextend -L +10G /dev/[vg]/[lv]
+}
+
+resize_filesystem() {
+    # df -h stil see old disk space we need to tell file system
+    resize2fs /dev/[vg]/[lv]
+}
+
+main() {
+    check_root
+    check_new_disk
+    resize_partition
+    resize_pv
+    extend_lv
+    resize_filesystem
+}
+
+main "$@"
