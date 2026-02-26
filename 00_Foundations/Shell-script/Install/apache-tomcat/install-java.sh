2b393d928fd0b855a2000c2a15cbcf5965baa377
Chonrachart <chonrachart.sieng@gmail.com>
2026-02-26 14:05:19 +0700

    install-java: add syslink

--- 00_Foundations/Shell-script/Install/apache-tomcat/install-java.sh
+++ 00_Foundations/Shell-script/Install/apache-tomcat/install-java.sh
@@ -5,6 +5,7 @@ set -e
 JAVA_VERSION="21.0.10"
 INSTALL_DIR="/opt/java"
 INSTALL_TARGET="$INSTALL_DIR/jdk-$JAVA_VERSION"
+SYMLINK="$INSTALL_DIR/java"
 TAR_FILE="$INSTALL_DIR/jdk-21_linux-x64_bin.tar.gz"
 URL="https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz"
 
@@ -34,6 +35,9 @@ extract_java() {
     else
         echo "Java already installed."
     fi
+
+    echo "[INFO] Updating symlink..."
+    ln -sfn "$INSTALL_TARGET" "$SYMLINK"
 }
 
 verify_java() {
@@ -45,8 +49,13 @@ main() {
     make_dir
     download_gz
     extract_java
+    echo " ==============================================="
+    echo "[INFO] You can check real java verion from this"
+    echo " ==============================================="
     verify_java
-    echo " This fix name version to 21.0.10 even it not check first!!!!"
+    echo " -----------------------------------------------"
+    echo "[INFO] Symlink points to: $INSTALL_TARGET"
+    echo "[WARNING]This fix name version to 21.0.10 even it not check first!!!!"
 }
 
 main "$@"
