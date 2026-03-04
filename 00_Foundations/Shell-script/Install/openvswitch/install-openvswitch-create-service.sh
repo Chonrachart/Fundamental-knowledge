fca51da10cccddb23a4ef97ecc4923d88c854bc7
Chonrachart <chonrachart.sieng@gmail.com>
2026-03-04 16:38:07 +0700

    install-openvswitch-create-service.sh

--- 00_Foundations/Shell-script/Install/openvswitch/install-openvswitch-create-service.sh
+++ 00_Foundations/Shell-script/Install/openvswitch/install-openvswitch-create-service.sh
@@ -1,16 +1,16 @@
-# In /etc/systemd/system/
+# In /etc/systemd/system/[service-name].service
 
-[Unit]
+[Unit]  # this section define when and what order service to start
 Description=OpenVSwitch [service-name] Service
-Wants=network-online.target
-After=network-online.target
+Wants=network-online.target # soft dependency (not strict require if it fail service still tries to start)
+After=network-online.target # strat this service after this
 
-[Service]
-Type=oneshot
-User=root
-RemainAfterExit=yes
-ExecStart=/etc/network/openvswitch/[setup-file].bash
-ExecStop=/usr/bin/ovs-vsctl del-br [bridge-name]
+[Service] # this section define how service run
+Type=oneshot # how service run (oneshot mean runas boot or use for setup)
+User=root # run as user....
+RemainAfterExit=yes # this is tell systemd to keeps the service state as active even the script is finish
+ExecStart=/etc/network/openvswitch/[setup-file].bash # run which script
+ExecStop=/usr/bin/ovs-vsctl del-br [bridge-name] # if service stop what to do
 
-[Install]
-WantedBy=multi-user.target
+[Install] # this section define how service should start
+WantedBy=multi-user.target # normal server boot 
